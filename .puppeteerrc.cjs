8b7beae9e6ab514f78da0dde093b1e395fdeaf65
hitesh <hiteshkr759@gmail.com>
2023-10-30 00:56:18 +0530

    EX File provide

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,5 +1,5 @@
 const {join} = require('path');
-const {executablePath} = require('puppeteer')
+const { executablePath } = require('puppeteer');
 // console.log('TRYING TO FETCH BROWSER')
 // let revisionInfo = await browserFetcher.download('884014');
 
@@ -9,8 +9,11 @@ const {executablePath} = require('puppeteer')
 module.exports = {
   // Changes the cache location for Puppeteer.
  // cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  executablePath: executablePath(),
+ ///tmp/hiteshkr759-starter-nestjs/.cache/puppeteer/chrome/linux-118.0.5993.70
+ ///tmp/hiteshkr759-starter-nestjs/.cache/puppeteer/chrome/linux-118.0.5993.70/chrome-linux64/chrome
+  executablePath: 'tmp/hiteshkr759-starter-nestjs/.cache/puppeteer/chrome/linux-118.0.5993.70/chrome-linux64/chrome',
   headless: 'new',
+  ignoreDefaultArgs: ['--disable-extensions'],
  // downloadBaseUrl : 'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing',
   //args:  ["--no-sandbox", "--disabled-setupid-sandbox"],
 };
