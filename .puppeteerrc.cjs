6706302c2390677d772f22708332b79c48358e78
hitesh <hiteshkr759@gmail.com>
2023-10-29 23:02:44 +0530

    k

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,4 +1,8 @@
 const {join} = require('path');
+puppeteer = require('puppeteer-core');
+// console.log('TRYING TO FETCH BROWSER')
+const browserFetcher = puppeteer.createBrowserFetcher();
+let revisionInfo = await browserFetcher.download('884014');
 
 /**
  * @type {import("puppeteer").Configuration}
@@ -6,7 +10,7 @@ const {join} = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-  executablePath: '/usr/bin/chromium-browser',
+  executablePath: revisionInfo.executablePath,
   headless:false,
-  args: ["--no-sandbox"]
+  args:  ["--no-sandbox", "--disabled-setupid-sandbox"],
 };
