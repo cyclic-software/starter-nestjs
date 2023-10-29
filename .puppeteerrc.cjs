adeeb23e36eb947e8fc6c8e1d668814e8f6bb329
hitesh <hiteshkr759@gmail.com>
2023-10-29 22:51:24 +0530

    tr

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -6,4 +6,6 @@ const {join} = require('path');
 module.exports = {
   // Changes the cache location for Puppeteer.
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  headless:false,
+  args: ["--no-sandbox"]
 };
