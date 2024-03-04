3906cf44ea471a94289e74afc8045c7ae906093b
dbruidb <162143047+dbruidb@users.noreply.github.com>
2024-03-04 11:31:51 -0500

    Update app.controller.spec.ts

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   describe('getHello', () => {
     it('should return "Hello World!"', () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe('Hello World! 99');
     });
   });
 });
