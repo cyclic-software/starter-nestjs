21e033bb61f63577ae679f13592a200f7f745c64
dbruidb <162143047+dbruidb@users.noreply.github.com>
2024-03-14 18:26:39 -0500

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const config_1 = require("@nestjs/config");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.enableCors({
-        origin: ["http://nrex-league.x10.mx"],
+        origin: ["http://formato_nr.x10.mx"],
         methods: ["GET", "POST"],
         credentials: true
     });
